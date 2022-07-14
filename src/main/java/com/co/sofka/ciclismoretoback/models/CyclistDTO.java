7e2d80b90f00bf911422f142388ac2f8c25eac27
Aura Russill <auracris1996@hotmail.com>
2022-07-14 10:06:58 -0500

    ADD:Config, Models
    SE agrego la configutacion de cors y los dtos para cada entidad

--- src/main/java/com/co/sofka/ciclismoretoback/models/CyclistDTO.java
+++ src/main/java/com/co/sofka/ciclismoretoback/models/CyclistDTO.java
@@ -1,4 +1,27 @@
 package com.co.sofka.ciclismoretoback.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.validation.constraints.NotBlank;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class CyclistDTO {
+
+    private String cyclistId;
+    private Integer riderNumber;
+
+    @NotBlank
+    private String name;
+
+    @NotBlank
+    private String lastName;
+
+    private String teamId;
+    private String nationality;
 }
--- src/main/java/com/co/sofka/ciclismoretoback/models/TeamDTO.java
+++ src/main/java/com/co/sofka/ciclismoretoback/models/TeamDTO.java
@@ -1,4 +1,36 @@
 package com.co.sofka.ciclismoretoback.models;
 
+import com.co.sofka.ciclismoretoback.collections.Cyclist;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.util.List;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class TeamDTO {
+
+    private String teamId;
+
+    private String codeTeam;
+
+    @NotBlank
+    private String name;
+
+    @NotBlank
+    private String country;
+
+    @NotNull
+    @Length.List( {
+        @Length(min = 1, message = "Debe tener al menos un ciclista"),
+        @Length(max = 4, message = "Debe tener máximo 8 ciclistas")
+    } )
+    private List<Cyclist> cyclists;
 }
