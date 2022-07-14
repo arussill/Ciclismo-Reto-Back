8bbe6b97cfeaeb8cfa26499451c9283470889a98
Aura Russill <auracris1996@hotmail.com>
2022-07-14 09:37:17 -0500

    ADD:Collections, Teams, Cyclists
    Se agrego los atributos a las entidades a utilizar

--- src/main/java/com/co/sofka/ciclismoretoback/collections/Cyclist.java
+++ src/main/java/com/co/sofka/ciclismoretoback/collections/Cyclist.java
@@ -1,4 +1,25 @@
 package com.co.sofka.ciclismoretoback.collections;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Document(collection = "cyclists")
 public class Cyclist {
+
+    @Id
+    private Integer cyclistId;
+    private String name;
+    private String lastName;
+    private String teamId;
+    private String nationality;
+
+
 }
--- src/main/java/com/co/sofka/ciclismoretoback/collections/Team.java
+++ src/main/java/com/co/sofka/ciclismoretoback/collections/Team.java
@@ -1,4 +1,25 @@
 package com.co.sofka.ciclismoretoback.collections;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Document(collection = "Teams")
 public class Team {
+
+    @Id
+    private String teamId;
+    private String name;
+    private String country;
+    private List<Cyclist> cyclists;
+
 }
